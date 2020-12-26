923a81d98be72129caada02bdb2a7855bd54f5d8
vyaskartik20 <vyas.2@iitj.ac.in>
2020-12-27 00:50:00 +0530

    further improvement of text recognition  using pyspellchecker library

---
+++ api1/correct_pyspellchecker.py
@@ -0,0 +1,17 @@
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+filePath="D:\BTP-2\\api1\\inputFinal.txt"
+
+f = open(filePath).read()
+
+file2=open("outputFinal2.txt","w")
+
+for word in f.split():
+    # do something with word
+    print (word + '   ' + spell.correction(word) )
+    file2.write(spell.correction(word))
+    file2.write(' ')
+  
+file2.close()
