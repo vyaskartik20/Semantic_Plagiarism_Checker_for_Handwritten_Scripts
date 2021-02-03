5054b683f310bbde90ab365d83952089f68a6f24
aditya kumar <kumar.92@iitj.ac.in>
2021-02-03 13:15:26 +0530

    trained model on sick dataset

--- semantic/sick_dataset/source_pytorch/model.py
+++ semantic/sick_dataset/source_pytorch/model.py
@@ -1,67 +1,161 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+# pytorch mlp for regression
+from numpy import vstack
+from numpy import sqrt
+from pandas import read_csv
+from sklearn.metrics import mean_squared_error
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch import Tensor
+from torch.nn import Linear
+from torch.nn import Sigmoid
+from torch.nn import Module
+from torch.optim import SGD
+from torch.nn import MSELoss
+from torch.nn.init import xavier_uniform_
+
+# dataset definition
 
 
-## DONE: Complete this classifier
-class BinaryClassifier(nn.Module):
-    """
-    Define a neural network that performs binary classification.
-    The network should accept your number of features as input, and produce 
-    a single sigmoid value, that can be rounded to a label: 0 or 1, as output.
+class CSVDataset(Dataset):
+    # load the dataset
+    def __init__(self, path):
+        # load the csv file as a dataframe
+        df = read_csv(path, header=None)
+        # store the inputs and outputs
+        self.X = df.values[:, 1:].astype('float32')
+        self.y = df.values[:, 0].astype('float32')
+        # ensure target has the right shape
+        self.y = self.y.reshape((len(self.y), 1))
 
-    Notes on training:
-    To train a binary classifier in PyTorch, use BCELoss.
-    BCELoss is binary cross entropy loss, documentation:
-    https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
-    """
+    # number of rows in the dataset
+    def __len__(self):
+        return len(self.X)
 
-    ## DONE: Define the init function, the input params are required
-    # (for loading code in train.py to work)
-    def __init__(self, input_features, hidden_dim, output_dim, dropout=0.2):
-        """
-        Initialize the model by setting up linear layers.
-        Use the input parameters to help define the layers of your model.
+    # get a row at an index
+    def __getitem__(self, idx):
+        return [self.X[idx], self.y[idx]]
 
-        Arguments:
-        :param input_features: the number of input features in your \
-            training/test data
-        :param hidden_dim: define the number of nodes in the hidden layer(s)
-        :param output_dim: the number of outputs you want to produce
-        :param dropout: probability of an element to be zeroed. Default: 0.2
-        """
-        super(BinaryClassifier, self).__init__()
+    # get indexes for train and test rows
+    def get_splits(self, n_test=0.33):
+        # determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        # calculate the split
+        return random_split(self, [train_size, test_size])
 
-        nodes = []
-        nodes.append(input_features)
-        if type(hidden_dim) == int:
-            nodes.append(hidden_dim)
-        elif type(hidden_dim) == list:
-            nodes.extend(hidden_dim)
-        nodes.append(output_dim)
+# model definition
 
-        self.module_list = nn.ModuleList()
-        for n_in, n_out in zip(nodes[:-1], nodes[1:]):
-            self.module_list.append(nn.Linear(n_in, n_out))
 
-        self.dropout = nn.Dropout(dropout)
+class MLP(Module):
+    # define model elements
+    def __init__(self, n_inputs):
+        super(MLP, self).__init__()
+        # input to first hidden layer
+        self.hidden1 = Linear(n_inputs, 10)
+        xavier_uniform_(self.hidden1.weight)
+        self.act1 = Sigmoid()
+        # second hidden layer
+        self.hidden2 = Linear(10, 8)
+        xavier_uniform_(self.hidden2.weight)
+        self.act2 = Sigmoid()
+        # third hidden layer and output
+        self.hidden3 = Linear(8, 1)
+        xavier_uniform_(self.hidden3.weight)
 
-    ## DONE: Define the feedforward behavior of the network
-    def forward(self, x):
-        """
-        Perform a forward pass of our model on input features, x.
+    # forward propagate input
+    def forward(self, X):
+        # input to first hidden layer
+        X = self.hidden1(X)
+        X = self.act1(X)
+        # second hidden layer
+        X = self.hidden2(X)
+        X = self.act2(X)
+        # third hidden layer and output
+        X = self.hidden3(X)
+        return X
 
-        Arguments:
-        :param x: A batch of input features of size (batch_size,input_features)
+# prepare the dataset
 
-        Return:
-        :return: A single, sigmoid-activated value as output
-        """
 
-        # define the feedforward behavior
-        for layer in self.module_list[:-1]:
-            x = F.relu(layer(x))
-            x = self.dropout(x)
-        x = self.module_list[-1](x)
+def prepare_data(path):
+    # load the dataset
+    dataset = CSVDataset(path)
+    # calculate split
+    train, test = dataset.get_splits()
+    # prepare data loaders
+    train_dl = DataLoader(train, batch_size=32, shuffle=True)
+    test_dl = DataLoader(test, batch_size=1024, shuffle=False)
+    return train_dl, test_dl
 
-        return torch.sigmoid(x)
+# train the model
+
+
+def train_model(train_dl, model):
+    # define the optimization
+    criterion = MSELoss()
+    optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    # enumerate epochs
+    for epoch in range(100):
+        # enumerate mini batches
+        for i, (inputs, targets) in enumerate(train_dl):
+            # clear the gradients
+            optimizer.zero_grad()
+            # compute the model output
+            yhat = model(inputs)
+            # calculate loss
+            loss = criterion(yhat, targets)
+            # credit assignment
+            loss.backward()
+            # update model weights
+            optimizer.step()
+
+# evaluate the model
+
+
+def evaluate_model(test_dl, model):
+    predictions, actuals = list(), list()
+    for i, (inputs, targets) in enumerate(test_dl):
+        # evaluate the model on the test set
+        yhat = model(inputs)
+        # retrieve numpy array
+        yhat = yhat.detach().numpy()
+        actual = targets.numpy()
+        actual = actual.reshape((len(actual), 1))
+        # store
+        predictions.append(yhat)
+        actuals.append(actual)
+    predictions, actuals = vstack(predictions), vstack(actuals)
+    # calculate mse
+    mse = mean_squared_error(actuals, predictions)
+    return mse
+
+# make a class prediction for one row of data
+
+
+def predict(row, model):
+    # convert row to data
+    row = Tensor([row])
+    # make prediction
+    yhat = model(row)
+    # retrieve numpy array
+    yhat = yhat.detach().numpy()
+    return yhat
+
+
+# prepare the data
+path = 'train.csv'
+train_dl, test_dl = prepare_data(path)
+print(len(train_dl.dataset), len(test_dl.dataset))
+# define the network
+model = MLP(12)
+# train the model
+train_model(train_dl, model)
+# evaluate the model
+mse = evaluate_model(test_dl, model)
+print('MSE: %.3f, RMSE: %.3f' % (mse, sqrt(mse)))
+# make a single prediction (expect class=1)
+row = [1, 0.818181818, 0.9701425, 0.976480793, 1, 0, 1,
+       0.976947257, 0.954986513, 78.78787879, 0.938977063, 0.998986829]
+yhat = predict(row, model)
+print('Predicted: %.3f' % yhat)
