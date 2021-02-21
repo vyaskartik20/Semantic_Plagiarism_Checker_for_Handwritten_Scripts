8b9f25abff9c17e713fc5d5871445d2aacdd8c8e
aditya kumar <kumar.92@iitj.ac.in>
2021-02-21 23:12:39 +0530

    sentence bert added

--- semantic/sick_dataset_Experiment/source_pytorch/distinctFeatures/sentenceBERT.py
+++ semantic/sick_dataset_Experiment/source_pytorch/distinctFeatures/sentenceBERT.py
@@ -1,5 +1,6 @@
 # pip install sentence-transformers
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
 import math
 model = SentenceTransformer('bert-base-nli-mean-tokens')
 
@@ -13,13 +14,15 @@ sentences = ["I ate dinner.",
 
 sentence_embeddings = model.encode(sentences)
 
-print('Sample BERT embedding vector - length', len(sentence_embeddings[0]))
-print('Sample BERT embedding vector - note includes negative values',
-      sentence_embeddings[0])
+# print('Sample BERT embedding vector - length', len(sentence_embeddings[0]))
+# print('Sample BERT embedding vector - note includes negative values',
+#       sentence_embeddings[0])
 
 query = "I had pizza and pasta"
 query_vec = model.encode([query])[0]
 
+
 for sent in sentences:
-    sim = cosine(query_vec, model.encode([sent])[0])
+    sim = cosine_similarity(query_vec.reshape(
+        1, -1), model.encode([sent])[0].reshape(1, -1))
     print("Sentence = ", sent, "; similarity = ", sim)
